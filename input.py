9e0b02e91068772871fb92eee7dafe0f255ebe55
Pranita kamble <107885176+pranu1111@users.noreply.github.com>
2023-10-17 10:36:33 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,4 @@
+a = str(input("Enter your name:"))
+b = int(input("Enter your age:"))
+c = float(input("Enter your age:"))
+print("Your entered name is :", a ,",","your entered age is :", b , "," ,"your percentage is :", c)
